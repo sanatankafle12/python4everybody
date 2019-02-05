43addef41d25ca0ca257d7d6efd0a1a79da4f960
sanatankafle12 <42962016+sanatankafle12@users.noreply.github.com>
2019-02-05 14:34:19 +0545

    variables.py	  @sanatankafle12 Commit changes Commit summary
    variables practice

---
+++ variables.py
@@ -0,0 +1,20 @@
+#ex_2
+print('Welcome to ex_2')
+name=raw_input('Enter your name: ')
+print('Hello '+name)
+#ex_3
+print('Welcome to ex_3')
+hours=input('Enter no of hours: ')
+rate=input('Enter rate per hour: ')
+total_pay=hours*rate
+print(total_pay)
+#ex_4
+print('Welcome to ex_4')
+width=17
+height=12.0
+a=width//2
+b=width/2.0
+c=height/3
+d=1+2*5
+print(a,b,c,d)
+print(type(a),type(b),type(c),type(d))
