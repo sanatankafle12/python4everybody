9a9f84f97c0494fb58ea6b166e00fe8342805e6a
Sanatan kafle <kaflesanatan100@gmail.com>
2019-02-07 17:47:58 +0545

    slicing of some lines containing numbers and calculating the averages

--- files.py
+++ files.py
@@ -1,4 +1,19 @@
-fhand=input('Enter a file name: ')
-handle=open(fhand)
-for line in handle:
-    print(line.upper())
+# fhand=input('Enter a file name: ')
+# handle=open(fhand)
+# for line in handle:
+#     print(line.upper())
+# # ex 03
+file=input('Enter a file name: ')
+handle=open(file)
+total=0
+count=0
+for lines in handle:
+    lines=lines.rstrip()
+    if lines.startswith('X-DSPAM-Confidence: '):
+        z=lines.find(':')
+        numbers=float(lines[z+1:])
+        count=count+1
+        total=total+numbers
+print(total)
+print(count)
+print(total/count)
