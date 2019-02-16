2b62cbb37770dbd8c35276e4b9238445f75c7344
Sanatan kafle <kaflesanatan100@gmail.com>
2019-02-16 09:17:16 +0545

    Using function to find amount payable and grade received

--- functions.py
+++ functions.py
@@ -1,38 +1,45 @@
-import random
 
-for i in range(10):
-    x = random.randint(1,10)
-    print(x)
+#ex_1
+def computepay(hours,rate):
 
+    if hours>40:
+        pay=hours*rate*1.5
+    else:
+        pay=hours*rate
 
+    print(pay)
+while True:
 
+    hours=input('Enter number of hours: ')
+    rate=input('Enter rate per hour: ')
+    try:
+        hours=float(hours)
+        rate=float(rate)
+        break
+    except:
+        print('Please Enter a numerical value.')
+        continue
 
+print(computepay(hours,rate))
+#ex_2
+def computegrade(score):
 
-
-
-# #ex_1
-# def computepay(hours,rate):
-#     if hours>40:
-#         pay=hours*rate*1.5
-#     else:
-#         pay=hours*rate
-#
-#     print(pay)
-# computepay(77,2)
-# #ex_2
-# def computegrade(score):
-#     try:
-#         score=float(score)
-#         if score>1:
-#             return('Out of range')
-#         elif score>=0.9:
-#             return('A')
-#         elif score>=0.8:
-#             return('B')
-#         elif score>=0.7:
-#             return('C')
-#         elif score<0.7:
-#             return('D')
-#     except:
-#         return('Plese Enter a numeric value')
-# print(computegrade(0.8))
+        if score>1:
+            return('Out of range')
+        elif score>=0.9:
+            return('A')
+        elif score>=0.8:
+            return('B')
+        elif score>=0.7:
+            return('C')
+        elif score<0.7:
+            return('D')
+while True:
+    score=input('Enter the score received: ')
+    try:
+        score=float(score)
+        break
+    except:
+        print('Please Enter a numerical value.')
+        continue
+print(computegrade(0.8))
