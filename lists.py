38b5b9f6234eae11175daf0e63be2b131cf98ec8
Sanatan kafle <kaflesanatan100@gmail.com>
2019-02-16 10:12:15 +0545

    Using lists for various programmes

--- lists.py
+++ lists.py
@@ -1,4 +1,7 @@
-#ex1
+name=input('Enter youe name : ')
+print('Welcome ',name,'\nHere are some exercises that consists of various use of list to perform various tasks. \nLets get started.')
+#ex1 extracting a list of middle values of the given list.
+print('This prints out all the values in list excluding the first and last value')
 chop=[1,2,3,4,5,6]
 middle=[]
 for i in chop:
@@ -6,7 +9,8 @@ for i in chop:
         middle.append(i)
 print(middle)
 
-#ex2
+#ex2 Using list and file
+print('This prints out the third word from line that starts with "From" in given file.')
 fhand = open('mbox.txt')
 count = 0
 for line in fhand:
@@ -14,8 +18,11 @@ for line in fhand:
     # print 'Debug:', words
     if len(words) != 0 and words[0] == 'From':
         print(words[2])
+        count=count+1
+print(count)
+#ex03 Using list to remove repeated words in a file
+print('This prints out a list of words in a file without duplicates')
 
-ex03
 file=open('romeo.txt')
 list=[]
 fhand=file.read()
@@ -26,7 +33,8 @@ for word in words:
 list.sort()
 print(list)
 
-ex04
+#ex04 Same as ex_02
+print('This prints the total number of lines starting with "From: "')
 file=input('Enter a file name: ')
 hand=open(file)
 count=0
@@ -37,11 +45,12 @@ for lines in hand:
     if lines.startswith('From: '):
         count=count+1
         line=lines.split()
-        print(line[1])
+        line[1]
 print(count)
 
 
-#ex05
+#ex05 Using lists to find out highest and lowest values
+print('This prints out the max and min value of entered numbers. \nPlease Enter done when your list is completed.')
 z=[]
 while True:
     inp=input('Enter a no: ')
